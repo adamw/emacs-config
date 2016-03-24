03c5c816625207888166a3dcbdca1a3f2249e211
adamw <adam@warski.org>
2016-03-24 22:11:23 +0100

    Neotree enter+hide + peek

--- config/projects.el
+++ config/projects.el
@@ -12,6 +12,26 @@
 
 (setq neo-theme 'arrow)
 
-;; https://github.com/jaypei/emacs-neotree/issues/77
-(add-hook 'neo-enter-hook (lambda (type x y)
-                            (if (equal type 'file) (neotree-hide))))
+;; https://github.com/jaypei/emacs-neotree/issues/77 + https://github.com/hemmvm/dotemacs/blob/master/site-lisp/util--neotree.el
+(defun custom-neotree-enter-hide ()
+  (interactive)
+  (neotree-enter)
+  (neotree-hide)
+  )
+
+(defun custom-neotree-peek ()
+  (interactive)
+  (let ((neo-window (neo-global--get-window)))
+    (neotree-enter)
+    (select-window neo-window))
+  )
+
+(add-hook
+ 'neotree-mode-hook
+ (lambda ()
+   (define-key neotree-mode-map (kbd "RET") 'custom-neotree-enter-hide)))
+
+(add-hook
+ 'neotree-mode-hook
+ (lambda ()
+   (define-key neotree-mode-map (kbd "TAB") 'custom-neotree-peek)))
