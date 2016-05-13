cdf66ff13ab4f4bf938f57f6ff787e449686ef85
adamw <adam@warski.org>
2016-05-13 15:49:02 -0400

    Flycheck

--- config/init-flycheck.el
+++ config/init-flycheck.el
@@ -3,6 +3,4 @@
   (global-flycheck-mode t)
   (setq-default flycheck-disabled-checkers
                 (append flycheck-disabled-checkers '(javascript-eslint)))
-  (setq-default flycheck-idle-change-delay 2.0)
-  (setq-default flycheck-check-syntax-automatically '(idle-change))
   (setq flycheck-jshintrc "~/.emacs.d/.jshintrc"))
