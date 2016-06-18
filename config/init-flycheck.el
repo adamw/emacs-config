e542d2dc9d300223bc47c0466414cc46c2a9b9e4
adamw <adam@warski.org>
2016-06-18 15:47:47 +0200

    Colors

--- config/init-flycheck.el
+++ config/init-flycheck.el
@@ -1,9 +1,15 @@
-(global-set-key (kbd "M-n") 'next-error)
-(global-set-key (kbd "M-p") 'previous-error)
-
 (use-package flycheck
+  :bind (("M-n" . flycheck-next-error)
+         ("M-p" . flycheck-previous-error))
   :config
   (global-flycheck-mode t)
   (setq-default flycheck-disabled-checkers
                 (append flycheck-disabled-checkers '(javascript-eslint)))
   (setq flycheck-jshintrc "~/.emacs.d/.jshintrc"))
+
+(use-package flycheck-color-mode-line
+  :config
+  (eval-after-load "flycheck"
+    '(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
+  (set-face-foreground 'flycheck-color-mode-line-error-face "#870000")
+  (set-face-background 'flycheck-color-mode-line-warning-face "#707070"))
