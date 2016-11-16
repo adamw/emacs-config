46ba8a62ece646ade1d790d103025a739415456a
adamw <adam@warski.org>
2016-11-16 13:23:54 +0100

    Try to address max-specpdl-size issues

--- init.el
+++ init.el
@@ -1,9 +1,7 @@
+(setq max-lisp-eval-depth 1500
+      max-specpdl-size    3000)
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
+;(package-initialize)
 
 (add-to-list 'load-path "~/.emacs.d/config/")
 (add-to-list 'load-path "~/.emacs.d/custom/")
