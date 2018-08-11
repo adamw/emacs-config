36ea462e33f0b7872474f543e82e40b103ceded2
Adam Warski <adamw@adamw-vega.local>
2018-08-11 21:26:09 +0200

    Fix projectile

--- config/init-projectile.el
+++ config/init-projectile.el
@@ -1,5 +1,6 @@
 (use-package projectile
   :config
   (projectile-global-mode)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (add-to-list 'projectile-globally-ignored-directories "node_modules")
   :diminish projectile-mode)
